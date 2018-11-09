422529f082b2bcb965f8cf52ea7d0bff48d509a4
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-11-09 23:30:58 +0000

    Logging

--- plugins/global_functions.py
+++ plugins/global_functions.py
@@ -1,7 +1,19 @@
 from random import random
+import logging
 
 # Probability
 def probability(percent):
     outcome = random() < percent
-    print(outcome)
     return outcome
+
+# Logging
+async def log(event, info=""):
+    sender = await event.get_sender()
+    try: 
+        message = event.pattern_match.string
+    except AttributeError:
+        message = ""
+    logging.info(
+        f"""[{event.date.strftime('%c')}]:
+    [{sender.id}]@[{event.chat.id}] {sender.first_name}@{sender.username}: {message}
+    {info}""")
