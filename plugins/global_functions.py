f06e7462f06651f56146e2af50276439e506d7bf
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2019-01-09 19:11:28 +0000

    Cooldown, and less creepy logging

--- plugins/global_functions.py
+++ plugins/global_functions.py
@@ -1,5 +1,10 @@
+from collections import defaultdict
 from random import random
+import inspect
 import logging
+import asyncio
+import time
+
 
 # Probability
 def probability(percent):
@@ -9,11 +14,26 @@ def probability(percent):
 # Logging
 async def log(event, info=""):
     sender = await event.get_sender()
-    try: 
-        message = event.pattern_match.string
-    except AttributeError:
-        message = ""
+    command = inspect.currentframe().f_back.f_code.co_name
     logging.info(
         f"""[{event.date.strftime('%c')}]:
-    [{sender.id}]@[{event.chat_id}] {sender.first_name}@{sender.username}: {message}
-    {info}""")
+    [{sender.id}]@[{event.chat.id}] {sender.first_name}@{sender.username}: {command}
+    {info}""".rstrip())
+
+# Cooldown
+def cooldown(timeout):
+    def wrapper(function):
+        last_called = defaultdict(int)
+
+        async def wrapped(event, *args, **kwargs):
+            print(event.chat_id, event.chat.id)
+            current_time = time.time()
+            if current_time - last_called[event.chat_id] < timeout:
+                print(last_called)
+                time_left = round(timeout - (current_time - last_called[event.chat_id]), 1)
+                await log(event, f"Cooldown: {time_left}s")
+                return
+            last_called[event.chat_id] = current_time
+            return await function(event, *args, **kwargs)
+        return wrapped
+    return wrapper
