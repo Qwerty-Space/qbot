dbacbb9fb22b620bf4e2a5f2c2c163f4e40dac3b
Qwerty-Space <oliver_duce@live.co.uk>
2019-01-18 23:00:11 +0000

    chat.id to chat_id

--- plugins/global_functions.py
+++ plugins/global_functions.py
@@ -14,10 +14,11 @@ def probability(percent):
 # Logging
 async def log(event, info=""):
     sender = await event.get_sender()
+    # Get the name of the command sent to the bot:
     command = inspect.currentframe().f_back.f_code.co_name
     logging.info(
         f"""[{event.date.strftime('%c')}]:
-    [{sender.id}]@[{event.chat.id}] {sender.first_name}@{sender.username}: {command}
+    [{sender.id}]@[{event.chat_id}] {sender.first_name}@{sender.username}: {command}
     {info}""".rstrip())
 
 # Cooldown
